dbb649c5fb10c232e51d2d61137abe2a8a30b5b0
Alexander Milevski <info@w8r.name>
2020-11-15 20:36:52 +0100

    Updated  demo images

--- test/render.js
+++ test/render.js
@@ -17,52 +17,51 @@ var Polygon           = require('./helpers/polygon');
 //var geoData     = require('./fixtures/ne_50m_admin_0_countries.geojson');
 
 
-  console.time('svg');
-  var rendered =
-    geojson2svg(data, style, null, null, 'markupType')
-       .decorator('cloud', function (feature, coordinates, closed, bbox, fbounds) {
-         var radius = feature.properties.radius || 5;
-         console.time('wave');
-         var w = wave(coordinates, radius, closed, bbox, fbounds);
-         console.timeEnd('wave');
-         return w;
-       })
-      .render();
-  console.timeEnd('svg');
+console.time('svg');
+var rendered =
+  geojson2svg(data, style, null, null, 'markupType')
+      .decorator('cloud', function (feature, coordinates, closed, bbox, fbounds) {
+        var radius = feature.properties.radius || 5;
+        console.time('wave');
+        var w = wave(coordinates, radius, closed, bbox, fbounds);
+        console.timeEnd('wave');
+        return w;
+      })
+    .render();
+console.timeEnd('svg');
+
+fs.writeFileSync(
+  path.resolve(process.cwd(), 'demo/markup.svg'), rendered, {
+    encoding: 'utf-8'
+  });
+
+// render geo data
+fs.readFile(path.join(process.cwd(), 'test/fixtures/ne_50m_admin_0_countries.geojson'), {
+  encoding: 'utf-8'
+}, function (err, data) {
+  console.log('read');
+  var geoData = JSON.parse(data);
+
+  console.time('geo');
+  var rendered = geojson2svg(geoData, function (feature) {
+    return {
+      fill: 'rgb(' + [
+        Math.round(256 * Math.random()),
+        Math.round(256 * Math.random()),
+        Math.round(256 * Math.random())
+      ].join(',') + ')',
+      weight: 0.25,
+      color: '#333333'
+    };
+  }, null, function (coord) {
+    return [coord[0], 85-coord[1]];
+  }).render();
+  console.timeEnd('geo');
 
   fs.writeFileSync(
-    path.resolve(process.cwd(), 'demo/markup.svg'), rendered, {
+    path.resolve(process.cwd(), 'demo/lands.svg'), rendered, {
       encoding: 'utf-8'
     });
+  console.log('done');
+});
 
-/*
-  // render geo data
-  fs.readFile(path.join(process.cwd(), 'test/fixtures/ne_50m_admin_0_countries.geojson'), {
-    encoding: 'utf-8'
-  }, function (err, data) {
-    console.log('read');
-    var geoData = JSON.parse(data);
-
-    console.time('geo');
-    var rendered = geojson2svg(geoData, function (feature) {
-      return {
-        fill: 'rgb(' + [
-          Math.round(256 * Math.random()),
-          Math.round(256 * Math.random()),
-          Math.round(256 * Math.random())
-        ].join(',') + ')',
-        weight: 0.25,
-        color: '#333333'
-      };
-    }, null, function (coord) {
-      return [coord[0], 85-coord[1]];
-    }).render();
-    console.timeEnd('geo');
-
-    fs.writeFileSync(
-      path.resolve(process.cwd(), 'demo/lands.svg'), rendered, {
-        encoding: 'utf-8'
-      });
-    console.log('done');
-  });
-*/
